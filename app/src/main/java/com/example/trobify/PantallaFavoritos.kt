1b21b3386a9530167a1a36be5e5d994f1c679172
Sauuudi <iliasbox2@gmail.com>
2021-03-05 13:37:02 +0100

    cosas

---
+++ app/src/main/java/com/example/trobify/PantallaFavoritos.kt
@@ -0,0 +1,9 @@
+package com.example.trobify
+
+import androidx.appcompat.app.AppCompatActivity
+
+class PantallaFavoritos : AppCompatActivity() {
+
+    //aqui se pasa el user desde main, esto es solo una prueba
+    val usuario = User("pepe","garcia", "aaa", "paco123","paco111",456, mutableListOf("piso1","piso2"))
+}
--- app/src/main/java/com/example/trobify/User.kt
+++ app/src/main/java/com/example/trobify/User.kt
@@ -1,15 +1,9 @@
 package com.example.trobify
 
-class User constructor(var nombre: String,
-                       var apellidos: String,
-                       var correo: String,
-                       var id: String,
-                       var pass: String,
-                       var telefono: Int
-                       //var EstoDeberiaSerUnaListaDeFavoritos: String = "Lista de favoritos, no string, modificar luego",
-                       //var Imagen: String = "imagen, luego vemos como se mete"
-)
-{
+import android.widget.ImageView
 
+//cambiar list a inmueble y añadir imgview para imgPerfil
+class User(var nombre : String, var apellidos : String, var correo : String, var id : String, var pass : String, var telefono : Int,var listaFavoritos : MutableList<String>)
+{
 
 }
