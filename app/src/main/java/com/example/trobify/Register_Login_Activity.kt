6ae7fe7bf02961ee449cbc54024416b3cd52e28b
RUBÉN LÓPEZ ROSIQUE <ruloro1@upv.edu.es>
2021-03-05 12:17:58 +0100

    Actividad Registrar Login

---
+++ app/src/main/java/com/example/trobify/Register_Login_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.trobify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Register_Login_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register__login_)
+    }
+}
