5f03930b3ae2228012ba908d7b34915c96958d24
Sauuudi <iliasbox2@gmail.com>
2021-03-05 12:28:32 +0100

    class user

--- app/src/main/java/com/example/trobify/User.kt
+++ app/src/main/java/com/example/trobify/User.kt
@@ -3,9 +3,9 @@ package com.example.trobify
 class User constructor(var nombre: String,
                        var usuario: String,
                        var pass: String,
-                       var telefono: Int,
-                       var EstoDeberiaSerUnaListaDeFavoritos: String = "Lista de favoritos, no string",
-                       var Imagen: String = "imagen, luego vemos como se mete"
+                       var telefono: Int
+                       //var EstoDeberiaSerUnaListaDeFavoritos: String = "Lista de favoritos, no string, modificar luego",
+                       //var Imagen: String = "imagen, luego vemos como se mete"
 )
 {
 
