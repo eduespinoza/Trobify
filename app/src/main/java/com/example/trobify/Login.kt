699863b8b51098a9e7f7a76cda841f3baca50103
RUBÉN LÓPEZ ROSIQUE <ruloro1@upv.edu.es>
2021-03-08 17:58:29 +0100

    Login 1
    Crear la clase Login, hacer que main salte a Login

--- app/src/main/java/com/example/trobify/Login.kt
+++ app/src/main/java/com/example/trobify/Login.kt
@@ -3,9 +3,9 @@ package com.example.trobify
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Register_Login_Activity : AppCompatActivity() {
+class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register__login_)
+        setContentView(R.layout.activity_login)
     }
 }
