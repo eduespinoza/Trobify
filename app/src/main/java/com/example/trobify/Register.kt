5c2d6012a34885af41861e2d526b7d4c9a35b8af
RUBÉN LÓPEZ ROSIQUE <ruloro1@upv.edu.es>
2021-03-10 10:01:28 +0100

    Registar v2

--- app/src/main/java/com/example/trobify/Register.kt
+++ app/src/main/java/com/example/trobify/Register.kt
@@ -1,11 +1,22 @@
 package com.example.trobify
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 
 class Register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val bBack = findViewById<ImageButton>(R.id.buttonGoBack)
+        bBack.setOnClickListener {
+            val goBack = Intent(this, Login::class.java)
+            finish()
+            
+        }
+
     }
 }
