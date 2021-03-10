b3fbcfb5236e2c0ec902609705708296239a88a9
RUBÉN LÓPEZ ROSIQUE <ruloro1@upv.edu.es>
2021-03-10 10:34:53 +0100

    Login-Register v3

--- app/src/main/java/com/example/trobify/PantallaFavoritos.kt
+++ app/src/main/java/com/example/trobify/PantallaFavoritos.kt
@@ -5,5 +5,5 @@ import androidx.appcompat.app.AppCompatActivity
 class PantallaFavoritos : AppCompatActivity() {
 
     //aqui se pasa el user desde main, esto es solo una prueba
-    val usuario = User("pepe","garcia", "aaa", "paco123","paco111",456, mutableListOf("piso1","piso2"))
+
 }
--- app/src/main/java/com/example/trobify/User.kt
+++ app/src/main/java/com/example/trobify/User.kt
@@ -2,8 +2,20 @@ package com.example.trobify
 
 import android.widget.ImageView
 
-//cambiar list a inmueble y añadir imgview para imgPerfil
-class User(var nombre : String, var apellidos : String, var correo : String, var id : String, var pass : String, var telefono : Int,var listaFavoritos : MutableList<String>)
-{
+
+class User {
+
+    private var name : String = ""
+    private var surname : String = ""
+    private var email : String = ""
+    private var id : String = ""
+    private var pass : String = ""
+    private var telefono : Int = -1
+    private var listaFavoritos : MutableList<String> = mutableListOf()
+
+    class User(var nombre : String, var apellidos : String, var correo : String, var id : String,
+    var pass : String, var telefono : Int,var listaFavoritos : MutableList<String>){}
+
+
 
 }
