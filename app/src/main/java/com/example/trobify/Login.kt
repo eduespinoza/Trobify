ccb0103792b6a169b586966ca19d7e1a50910365
LrRuben <38402745+LrRuben@users.noreply.github.com>
2021-03-12 14:10:48 +0100

    Login + Register FINAL
    Terminada la implementación de registro y login

--- app/src/main/java/com/example/trobify/Login.kt
+++ app/src/main/java/com/example/trobify/Login.kt
@@ -3,22 +3,57 @@ package com.example.trobify
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import android.content.ContentValues.TAG
 
 class Login : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    var name:String? = null
+    var email:String? = null
+    var password:String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        auth = Firebase.auth
+        
+
         val buttonRegistrar = findViewById<Button>(R.id.buttonRegister)
         buttonRegistrar.setOnClickListener{
             val goRegister = Intent(this, Register::class.java)
             startActivity(goRegister)
         }
+
         val buttonAcceder = findViewById<Button>(R.id.buttonAcces)
         buttonAcceder.setOnClickListener{
-
-
+            email  = findViewById<EditText>(R.id.editTextLoginEmail).text.toString()
+            password  = findViewById<EditText>(R.id.editTextLoginPassword).text.toString()
+            
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Log.d(TAG, "signInWithEmail:success")
+                        val user = auth.currentUser
+                        updateUI(user)
+                    } else {
+                        Log.w(TAG, "signInWithEmail:failure", task.exception)
+                        Toast.makeText(baseContext, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
+                        updateUI(null)
+                    }
+                }
         }
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        println("Bienvenido: " )
+
     }
 }
--- app/src/main/java/com/example/trobify/User.kt
+++ app/src/main/java/com/example/trobify/User.kt
@@ -1,6 +1,15 @@
 package com.example.trobify
 
-import android.widget.ImageView
+import android.content.ContentValues.TAG
+import android.content.ContextWrapper
+import android.content.Intent
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import io.grpc.Context
 
 
 class User {
@@ -13,9 +22,15 @@ class User {
     private var telefono : Int = -1
     private var listaFavoritos : MutableList<String> = mutableListOf()
 
+
     class User(var nombre : String, var apellidos : String, var correo : String, var id : String,
     var pass : String, var telefono : Int,var listaFavoritos : MutableList<String>){}
 
+
+
+    private fun updateUI(user: FirebaseUser?) {
+
+    }
 
 
 }
