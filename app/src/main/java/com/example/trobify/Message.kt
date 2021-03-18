3d3149007b63df1a3f26eb41630330e3c81c0310
LrRuben <38402745+LrRuben@users.noreply.github.com>
2021-03-18 09:33:45 +0100

    Message

--- app/src/main/java/com/example/trobify/Message.kt
+++ app/src/main/java/com/example/trobify/Message.kt
@@ -1,63 +1,38 @@
 package com.example.trobify
 
-import android.widget.ImageView
-import android.widget.TextView
-import de.hdodenhof.circleimageview.CircleImageView
 
-open class Message {
-    lateinit var message : String
-    lateinit var name : String
-    private lateinit var profilePicture : String
-    lateinit var type_message : String
-    private lateinit var time : String
+class Message {
+    var textMessage : String? = null
+    var urlPicture : String? = null
+    var name : String? = null
+    var profilePicture : String? = null
+    var type_mensage : String? = null
 
-    constructor(){}
-    constructor(message : String, name : String, profilePicture : String, type_message : String, time : String){
-        this.message = message
+    constructor() {}
+    constructor(
+        textMessage : String?,
+        name : String?,
+        profilePicture : String?,
+        type_mensage : String?
+    ) {
+        this.textMessage = textMessage
         this.name = name
         this.profilePicture = profilePicture
-        this.type_message = type_message
-        this.time = time
+        this.type_mensage = type_mensage
     }
 
-
-    fun getMessage() : String {
-        return message
-    }
-
-    fun setMessage(message : String) {
-        this.message = message
-    }
-
-    fun getName() : String {
-        return name
-    }
-
-    fun setName(name : String) {
+    constructor(
+        textMessage : String?,
+        urlPicture : String?,
+        name : String?,
+        profilePicture : String?,
+        type_mensage : String?
+    ) {
+        this.textMessage = textMessage
+        this.urlPicture = urlPicture
         this.name = name
-    }
-
-    fun getprofilePictureMessage() : String{
-        return profilePicture
-    }
-
-    fun setprofilePictureMessage(profilePicture : String) {
         this.profilePicture = profilePicture
+        this.type_mensage = type_mensage
     }
 
-    fun get_type_message() : String {
-        return type_message
-    }
-
-    fun set_type_message(pictureMessage : String) {
-        this.type_message = type_message
-    }
-
-    fun getTime() : String {
-        return time
-    }
-
-    fun setTime(time : String) {
-        this.time = time
-    }
 }
--- app/src/main/java/com/example/trobify/MessageReception.kt
+++ app/src/main/java/com/example/trobify/MessageReception.kt
@@ -1,20 +1,22 @@
 package com.example.trobify
 
 class MessageReception : Message {
-    var time : Long? = null
+    var hora : Long? = null
 
     constructor() {}
-    constructor(time : Long?) {
-        this.time = time
+    constructor(hora : Long?) {
+        this.hora = hora
     }
 
     constructor(
-        message : String?,
-        urlPicture : String?,
-        name : String?,
-        profilePicture : String?,
-        type_message : String?,
-        time : Long?
-    ) : super(message!!, urlPicture!! , name!! , profilePicture!!, type_message!! ) { this.time = time }
+        mensaje : String?,
+        urlFoto : String?,
+        nombre : String?,
+        fotoPerfil : String?,
+        type_mensaje : String?,
+        hora : Long?
+    ) : super(mensaje, urlFoto, nombre, fotoPerfil, type_mensaje) {
+        this.hora = hora
+    }
 
 }
