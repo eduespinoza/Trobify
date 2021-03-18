b7f54ff2c044966f1df5fad6ca67889ec0661125
LrRuben <38402745+LrRuben@users.noreply.github.com>
2021-03-18 09:18:55 +0100

    Message_Card.xml

--- app/src/main/java/com/example/trobify/MessageHolder.kt
+++ app/src/main/java/com/example/trobify/MessageHolder.kt
@@ -1,11 +1,8 @@
 package com.example.trobify
 
-import  android.R.id
 import android.view.View
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import de.hdodenhof.circleimageview.CircleImageView
 
 
 class MessageHolder : RecyclerView.ViewHolder{
@@ -19,7 +16,7 @@ class MessageHolder : RecyclerView.ViewHolder{
     constructor(itemView : View) : super(itemView){
         name = itemView.findViewById<View>(R.id.profileName) as TextView
         message = itemView.findViewById<View>(R.id.txtMessage) as TextView
-        time = itemView.findViewById<View>(R.id.horaMensaje as TextView
+        time = itemView.findViewById<View>(R.id.timeInMessage as TextView
         //profilePicture = itemView.findViewById<View>(R.id.fotoPerfilMensaje) as CircleImageView
         //messagePicture = itemView.findViewById<View>(R.id.mensajeFoto) as ImageView
     }
