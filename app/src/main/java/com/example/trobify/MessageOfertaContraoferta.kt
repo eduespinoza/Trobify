700c463fa9c640683dfc97366a9de84f6e9217ae
diego04-rc <diego04-rc@users.noreply.github.com>
2021-03-18 11:14:49 +0100

    Revert "MensajeOferta v2"
    This reverts commit 1f3a21e137a8a753c6dd593c46c17b29f3fae64f.

--- app/src/main/java/com/example/trobify/MessageOfertaContraoferta.kt
+++ app/src/main/java/com/example/trobify/MessageOfertaContraoferta.kt
@@ -3,7 +3,6 @@ package com.example.trobify
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
@@ -17,23 +16,10 @@ class MessageOfertaContraoferta  : AppCompatActivity(){
 
         val buttonSendOffer = findViewById<Button>(R.id.buttonSendOffer)
 
-        val textNameInmueble = findViewById<TextView>(R.id.textNameInmueble)
-        textNameInmueble.setText()
-
-        val textFeaturesInmueble = findViewById<TextView>(R.id.textFeaturesInmueble)
-        textFeaturesInmueble.setText()
-
-        val textLocationInmueble = findViewById<TextView>(R.id.textLocationInmueble)
-        textLocationInmueble.setText()
-
-        val textPriceInmueble = findViewById<TextView>(R.id.textPriceInmueble)
-        textPriceInmueble.setText()
-
         buttonSendOffer.setOnClickListener {
             checkOffer()
             if(quantityOk == true){
-                createMessage()
-                sendMessage()
+
             }
         }
     }
@@ -48,14 +34,5 @@ class MessageOfertaContraoferta  : AppCompatActivity(){
                 setPositiveButton("Ok", null)
             }.show()
         }
-    }
-
-    private fun createMessage(){
-        val textOfferQuantity = findViewById<EditText>(R.id.textOfferQuantity)
-        Message().setMessage("(Nombre de la perosna) te ha enviado una oferta" + "Nombre del inmueble" + "Cantidad ofrecida: " + textOfferQuantity.toString())
-    }
-
-    private fun sendMessage(){
-        MessageSend()
     }
 }
