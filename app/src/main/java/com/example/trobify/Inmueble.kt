c7ac8f3dadfbe5e986b697d93b2fd29378c2864f
Sauuudi <iliasbox2@gmail.com>
2021-03-22 16:28:13 +0100

    AdaptadofichaInmuebleee

--- app/src/main/java/com/example/trobify/Inmueble.kt
+++ app/src/main/java/com/example/trobify/Inmueble.kt
@@ -4,6 +4,7 @@ import android.content.ContentValues.TAG
 import android.content.ContextWrapper
 import android.content.Intent
 import android.util.Log
+import android.widget.CheckBox
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
@@ -13,37 +14,74 @@ import io.grpc.Context
 
 
 class Inmueble{
+    var titulo : String? = null
     var id : String? = null
     var propietario : User? = null
     var numHabitaciones : Int? = null
     var numBanos : Int? = null
     var superficie : Int? = null
+
     var direccion : String? = null
     var tipoInmueble : String? = null
     var intencion : String? = null //vender o alquilar
     var precio : Int? = null
 
-    //var photos : MutableList<Int>  //cambiar tipo a img
+    //var photos : ArryList<>  //cambiar tipo a img
     var certificadoEnergetico : String? = null  //Desde la A hasta la G, hay que hacer que solo deje poner las letras posibles
     var descripcion : String? = null
-    var titulo : String? = null //se puede quitar
 
-    var estado : String? = null  //hay que ver esto aun para que solo deje poner los estados posibles
 
-    //EXTRAS 0 si 1 no se pueden añadir mas o quitar los que sobran
-    var caractertisicasList = mutableListOf(true, false)
+    var estado : String? = null
+
+    var parking : Boolean? = null
+    var ascensor : Boolean? = null
+    var amueblado : Boolean? = null
+    var calefaccion : Boolean? = null
+    var jardin : Boolean? = null
+    var piscina : Boolean? = null
+    var terraza : Boolean? = null
+    var trastero : Boolean? = null
+
     var caracteristicas : String? = null
+
     constructor( title: String,   price: Int,  surface: Int,  photos: Array<Int>){
         this.titulo = "Inmueble en " + title
         this.precio = price
         this.superficie = surface
     }
-    constructor( id : String?, propietario : User?,  numHabitaciones : Int?, superficie : Int?,
-                 direccion: String?, tipoInmueble: String?, intencion: String?, precio : Int?,
-                 certificadoEnergetico : String?, descripcion : String?, titulo : String? , estado: String?  ) {
+
+    constructor(
+        titulo : String?,
+        id : String?,
+        propietario : User?,
+        numHabitaciones : Int?,
+        numBanos : Int?,
+        superficie : Int?,
+
+        direccion : String?,
+        tipoInmueble : String?,
+        intencion : String?,
+        precio : Int?,
+
+        certificadoEnergetico : String?,
+        descripcion : String?,
+
+        estado : String?,
+
+        parking : Boolean?,
+        ascensor : Boolean?,
+        amueblado : Boolean?,
+        calefaccion : Boolean?,
+        jardin : Boolean?,
+        piscina : Boolean?,
+        terraza : Boolean?,
+        trastero : Boolean?
+    ) {
+        this.titulo = "Inmueble en " + titulo
         this.id = id
         this.propietario = propietario
         this.numHabitaciones = numHabitaciones
+        this.numBanos = numBanos
         this.superficie = superficie
         this.direccion = direccion
         this.tipoInmueble = tipoInmueble
@@ -51,17 +89,31 @@ class Inmueble{
         this.precio = precio
         this.certificadoEnergetico = certificadoEnergetico
         this.descripcion = descripcion
-        this.titulo = "Inmueble en " + direccion
-
-        this.caracteristicas = caracteristicasToString(caractertisicasList)
         this.estado = estado
-
+        this.parking = parking
+        this.ascensor = ascensor
+        this.amueblado = amueblado
+        this.calefaccion = calefaccion
+        this.jardin = jardin
+        this.piscina = piscina
+        this.terraza = terraza
+        this.trastero = trastero
+        this.caracteristicas = caracteristicasToString()
     }
 
-    fun caracteristicasToString( list : MutableList<Boolean> ) : String?{
-        var res = ""
+    fun caracteristicasToString( ) : String {
+        var res = "El piso dispone de "
 
-        return "hola"
+        if(parking == true){ res += "-parking " }
+        if(ascensor == true){ res += "-ascensor " }
+        if(amueblado == true){ res += "-amueblado " }
+        if(calefaccion == true){ res += "-calefaccion " }
+        if(jardin == true){ res += "-jardin " }
+        if(piscina == true){ res += "-piscina " }
+        if(terraza == true){ res += "-terraza " }
+        if(trastero == true){ res += "-trastero " }
+
+        return res
     }
 
 
