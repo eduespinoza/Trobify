b8500adad0e2aa05cc2925afbdbb9629a5f8ff22
ruben lopez rosique <rubenlopezrosique@gmail.com>
2021-04-29 20:27:19 +0200

    Interfaz Añadir Inmueble

---
+++ app/src/main/java/com/example/trobify/OfertarInmueble.kt
@@ -0,0 +1,15 @@
+package com.example.trobify
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class OfertarInmueble : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState : Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ofertar_inmueble)
+
+
+
+        }
+    }
