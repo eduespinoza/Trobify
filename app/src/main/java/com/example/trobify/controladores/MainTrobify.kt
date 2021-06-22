c968d0ed713d072bfe82c549ca6b77a3eff407fa
diego04-rc <diego04-rc@users.noreply.github.com>
2021-06-22 18:07:55 +0200

    commit

--- app/src/main/java/com/example/trobify/controladores/MainTrobify.kt
+++ app/src/main/java/com/example/trobify/controladores/MainTrobify.kt
@@ -66,7 +66,7 @@ open class MainTrobify : AppCompatActivity(), AdaptadorInmuebleBusqueda.OnItemCl
         super.onResume()
         if(filtrosAplicados == null && opcionesDeInicio == null){
         user = Database.obtenerUsuario(userId)
-        userFav = arrayListOf()
+        userFav = user.favorites!!
         mostrarInmuebles(inmueblesEnPantalla)
         }
     }
@@ -82,7 +82,7 @@ open class MainTrobify : AppCompatActivity(), AdaptadorInmuebleBusqueda.OnItemCl
         opcionesDeInicio = intent.extras!!.get("opciones") as ArrayList<String>?
         filtrosAplicados = intent.extras!!.get("filtros") as FiltrosBusqueda.filtros?
         user = Database.obtenerUsuario(userId)
-        userFav = arrayListOf()
+        userFav = user.favorites!!
         inmuebles = Database.obtenerInmuebles()
 
         if(opcionesDeInicio != null){
